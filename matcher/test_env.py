8173998aaa7257e120bf60e263bda04c21e812a9
talaabusoud <talaashrafsoud@gmail.com>
2025-06-25 06:48:08 +0300

    Ignore matcher/test folder

--- matcher/test_env.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-from pathlib import Path
-
-env_path = Path(__file__).resolve().parent / ".env"
-print("Path:", env_path)
-load_dotenv(dotenv_path=str(env_path))
-print("User:", os.getenv("DEJAVU_DB_USER"))
